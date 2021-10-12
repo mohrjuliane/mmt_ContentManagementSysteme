30eddbb5063b69035175542e4e3a8fb56c8d3a35
fhs45894 <fhs45894@fh-salzburg.ac.at>
2021-10-12 22:32:54 +0200

    fixed JS function for Burger-Toggle and Hero Image, box-decoration-break does not workgit add .!

--- AlexMayer/src/js/functions.js
+++ AlexMayer/src/js/functions.js
@@ -1,15 +1,33 @@
 function navSlide() {
-  const burger = document.querySelector(".hamburger hamburger--collapse");
-  //const nav = document.querySelector(".leiste");
+  //const burger = document.querySelector(".hamburger hamburger--collapse");
+  const burger = document.getElementById("hamburger-menu");
+
+  const nav = document.getElementById("navbar");
 
   //toggle Nav-Bar
-  burger.addEventListener("click", function () {
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("is-active");
     nav.classList.toggle("is-active");
-    console.log("clicked");
-
-    //Burger animation
-    //burger.classList.toggle("toggle");
   });
 }
 
-navSlide();
+function setCurrentPage() {
+  const links = document.getElementsByClassName("page");
+
+  for (let index = 0; index < links.length; index++) {
+    const element = links[index];
+
+    element.addEventListener("click", () => {
+      let current = document.getElementById("current");
+      element.setAttribute("id", "current");
+      current.removeAttribute("id", "current");
+    });
+  }
+}
+
+const constFunctions = () => {
+  navSlide();
+  setCurrentPage();
+};
+
+constFunctions();
