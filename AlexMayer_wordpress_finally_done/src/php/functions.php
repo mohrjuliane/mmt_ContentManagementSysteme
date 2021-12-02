f3f23e6f2e53b3c8c7d0d95908bae18ed83cf6e0
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2021-12-02 23:01:14 +0100

    Pages and Nav created

---
+++ AlexMayer_wordpress_finally_done/src/php/functions.php
@@ -0,0 +1,26 @@
+<?php
+function add_theme_scripts()
+{
+    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_script('script', get_template_directory_uri() . '/js/functions.js', array(), null, true);
+}
+
+function cc_mime_types($mimes)
+{
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
+add_filter('upload_mimes', 'cc_mime_types');
+add_theme_support('post-thumbnails');
+
+function register_my_menus()
+{
+    register_nav_menus(
+        array(
+            'main-menu' => __('Main Menu'),
+            'footer-menu' => __('Footer Menu')
+        )
+    );
+}
+add_action('init', 'register_my_menus');
